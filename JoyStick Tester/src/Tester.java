a902e343b714d6c0f57eef989096517ac3368702
Alex Dahlen <ahannegrefsdahlen@student.neumont.edu>
2017-02-17 14:04:58 -0700

    2/17/2017 1404

---
+++ JoyStick Tester/src/Tester.java
@@ -0,0 +1,14 @@
+
+import net.java.games.input.Controller;
+import net.java.games.input.ControllerEnvironment;
+import net.java.games.input.ControllerEvent;
+import net.java.games.input.ControllerListener;
+
+public class Tester implements ControllerListener{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
