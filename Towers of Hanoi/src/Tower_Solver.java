c34601633f52b29b23170b29da907bddcb9aaa3a
Alex Dahlen <ahannegrefsdahlen@student.neumont.edu>
2017-02-23 13:38:31 -0700

    2/23 13:38

--- Towers of Hanoi/src/Tower_Solver.java
+++ Towers of Hanoi/src/Tower_Solver.java
@@ -1,9 +1,11 @@
 
 public class Tower_Solver {
+	int moves = 0;
 
 	public static void main(String[] args) {
 		Tower_Solver towersOfHanoi = new Tower_Solver();
 		towersOfHanoi.moveDisc(Integer.parseInt(args[0]), "Left", "Middle", "Right");
+		towersOfHanoi.printLn(String.valueOf("Discs: " + Integer.parseInt(args[0]) + "\nMoves: " + towersOfHanoi.moves));
 	}
 
 	private void moveDisc(int n, String pegOne, String pegTwo, String pegThree) {
@@ -14,6 +16,7 @@ public class Tower_Solver {
 			printLn(pegOne + " ---> " + pegThree);
 			moveDisc(n - 1, pegTwo, pegOne, pegThree);
 		}
+		this.moves++;
 	}
 
 	private void printLn(String print) {
